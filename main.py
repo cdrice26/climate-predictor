e873032e06a3e0033a5b8e05dc89f085e03debbc
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-07-11 18:47:32 -0400

    Fixed spelling error and changed color for better accessibility.

--- main.py
+++ main.py
@@ -11,30 +11,38 @@ from sklearn.linear_model import LinearRegression
 # Necessary to fix requests
 pyodide_http.patch_all()
 
+
 # Converts the open-meteo name to a more human-readable format
 def get_parameter_name(parameter):
-    if parameter == 'temperature_2m_max': return 'High Temperature'
-    elif parameter == 'temperature_2m_min': return 'Low Temperature'
-    elif parameter == 'apparent_temperature_max': return 'Max Heat Index'
-    elif parameter == 'apparent_temperature_min': return 'Min Heat Index'
-    elif parameter == 'precipitation_sum': return 'Precipitation'
-    elif parameter == 'rain_sum': return 'Rainfall'
-    elif parameter == 'snowfall_sum': return 'Snowfall'
-    else: return 'Wind Speed'
+    if parameter == "temperature_2m_max":
+        return "High Temperature"
+    elif parameter == "temperature_2m_min":
+        return "Low Temperature"
+    elif parameter == "apparent_temperature_max":
+        return "Max Heat Index"
+    elif parameter == "apparent_temperature_min":
+        return "Min Heat Index"
+    elif parameter == "precipitation_sum":
+        return "Precipitation"
+    elif parameter == "rain_sum":
+        return "Rainfall"
+    elif parameter == "snowfall_sum":
+        return "Snowfall"
+    else:
+        return "Wind Speed"
+
 
 # Gets the latitude and longitude from search query location_string
 def geocode(location_string):
 
     # Get the response from the geocoding api
-    resp = requests.get(f'https://geocode.maps.co/search?q={location_string}')
+    resp = requests.get(f"https://geocode.maps.co/search?q={location_string}")
     json = resp.json()
 
     # Just store the coordinates in a dictionary
-    location = {
-        'lat': float(json[0]['lat']),
-        'lon': float(json[0]['lon'])
-    }
+    location = {"lat": float(json[0]["lat"]), "lon": float(json[0]["lon"])}
     return location
+
 
 # Gets the relevant weather data for a specified location
 def get_weather_data(location, parameter):
@@ -45,15 +53,16 @@ def get_weather_data(location, parameter):
     try:
         data = requests.get(url).json()
     except:
-        return 'Error fetching weather data'
-    
+        return "Error fetching weather data"
+
     # Get the dates and values
-    dates = data['daily']['time']
-    values = data['daily'][parameter]
+    dates = data["daily"]["time"]
+    values = data["daily"][parameter]
 
     # Replace None with 0
     for i in range(len(values)):
-        if values[i] == None: values[i] = 0
+        if values[i] == None:
+            values[i] = 0
 
     # Just keep the year
     for i in range(len(dates)):
@@ -62,48 +71,55 @@ def get_weather_data(location, parameter):
     # Get the indexes where the year changes
     change_indexes = []
     for i in range(len(dates)):
-        if not dates[i] == dates[i - 1]: change_indexes.append(i)
+        if not dates[i] == dates[i - 1]:
+            change_indexes.append(i)
     change_indexes.append(len(dates))
 
     # Calculate the average value for each year
     averages = []
     for i in range(len(change_indexes) - 1):
-        values_for_year = values[change_indexes[i]:change_indexes[i + 1]]
+        values_for_year = values[change_indexes[i] : change_indexes[i + 1]]
         averages.append(sum(values_for_year) / len(values_for_year))
 
     # Set the units
     units = None
-    if 'temperature' in parameter: units = '°C'
-    elif 'sum' in parameter: units = 'mm/day'
-    else: units = 'km/hr'
+    if "temperature" in parameter:
+        units = "°C"
+    elif "sum" in parameter:
+        units = "mm/day"
+    else:
+        units = "km/hr"
 
     return {
-        'start_year': int(dates[0]),
-        'end_year': int(dates[-1]) + 1,
-        'values': averages,
-        'units': units
+        "start_year": int(dates[0]),
+        "end_year": int(dates[-1]) + 1,
+        "values": averages,
+        "units": units,
     }
+
 
 # Graph the data and run linear regression
 def graph(data, parameter, location):
     plt.clf()
-    document.getElementById('canvas').innerHTML=''
+    document.getElementById("canvas").innerHTML = ""
     fig, ax = plt.subplots()
-    units = data['units']
-    xvals = range(data['start_year'], data['end_year'])
-    yvals = data['values']
+    units = data["units"]
+    xvals = range(data["start_year"], data["end_year"])
+    yvals = data["values"]
     reg = lin_reg(xvals, yvals)
     parameter_name = get_parameter_name(parameter)
-    plt.title(f'{parameter_name} in {location} ({units})')
+    plt.title(f"{parameter_name} in {location} ({units})")
     graph_data(xvals, yvals)
-    graph_reg(reg['m'], reg['b'], data['start_year'], data['end_year'])
+    graph_reg(reg["m"], reg["b"], data["start_year"], data["end_year"])
     return fig
+
 
 # Graph the data
 def graph_data(xvals, yvals):
     x = np.array(xvals)
     y = np.array(yvals)
     plt.plot(x, y)
+
 
 # Run linear regression and return slope and y-intercept
 def lin_reg(xvals, yvals):
@@ -112,23 +128,22 @@ def lin_reg(xvals, yvals):
     model = LinearRegression().fit(x, y)
     m = model.coef_
     b = model.intercept_
-    return {
-        'm': m,
-        'b': b
-    }
+    return {"m": m, "b": b}
+
 
 # Graph the regression line
 def graph_reg(m, b, xmin, xmax):
     fn = lambda x: m * x + b
-    f = np.vectorize(fn, excluded=['R', 'cc'])
+    f = np.vectorize(fn, excluded=["R", "cc"])
     x = np.arange(xmin - 20, xmax + 50)
     y = f(x)
     plt.plot(x, y)
 
-@when('click', '#go-button')
+
+@when("click", "#go-button")
 def run():
-    parameter = document.getElementById('parameter-select').value
-    location_string = document.getElementById('location-input').value
+    parameter = document.getElementById("parameter-select").value
+    location_string = document.getElementById("location-input").value
     location = geocode(location_string)
     data = get_weather_data(location, parameter)
     fig = graph(data, parameter, location_string)
